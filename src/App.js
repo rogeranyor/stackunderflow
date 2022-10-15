476f9018245b010055a26b9e560558f39555e83e
rogeranyor <ranorgra18@alumnes.ub.edu>
2022-10-15 20:27:16 +0200

    creo q funsiona

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import NavBar from './components/navBar';
 import Home from './components/Home';
-import './bootstrap/css/bootstrap.css'
-import './bootstrap/js/bootstrap.bundle.js'
-import './bootstrap/js/bootstrap.bundle.min.js'
+
 
 function App() {
   return (
